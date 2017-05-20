b5d95e646e07f93e959a5b826fdd7b1a343d56c5
trickertite <trickertite@gmail.com>
2017-05-20 18:44:58 +0500

    connection to mlab database made and route for get all tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://trickertite:tremendous0@ds147711.mlab.com:47711/my_task_list_tricker', ['tasks'])
 
 router.get('/tasks', function(req, res, next){
-  res.send('tasks');
+  db.tasks.find(function(err, tasks){
+    if (err) {
+      res.send(err);
+    }
+    res.json(tasks);
+  })
 });
 
 module.exports = router;
