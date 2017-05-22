55649fa13156845216f2f51d544d0197b116ffce
trickertite <trickertite@gmail.com>
2017-05-22 12:59:56 +0500

    tasks form added, server.js render view removed so now server server only static client folder

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ var port = 3000;
 var app = express();
 
 //View Engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+// app.engine('html', require('ejs').renderFile);
 
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'client')));
